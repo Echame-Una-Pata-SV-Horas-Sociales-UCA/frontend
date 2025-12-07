6f724a7be49f44f299bfa0a4cc3a46ba63ea0f92
cam-vasquez <00065221@uca.edu.sv>
2025-12-06 19:38:21 -0600

    :sparkles: Adds support page for "Red de Apoyo" network

---
+++ echaunapata/src/pages/hubNetwork.tsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/commons/navbar";
+import Footer from "../components/commons/footer";
+
+export default function HubNetwork() {
+  return (
+    <>
+      <Navbar />
+      {/* Add your HubNetwork page content here */}
+      <Footer />
+    </>
+  );
+}

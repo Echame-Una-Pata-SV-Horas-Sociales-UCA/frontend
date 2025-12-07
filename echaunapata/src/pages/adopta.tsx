66f2acf98edd3bef1869b0f715ddfb50711daa1a
Fernando Eguizabal <00124221@uca.edu.sv>
2025-12-06 19:28:34 -0600

    feat: remove unused imports from adopta and stats components

--- echaunapata/src/pages/adopta.tsx
+++ echaunapata/src/pages/adopta.tsx
@@ -4,7 +4,6 @@ import MiniHero from "../components/commons/miniHero";
 import RequisitosAdopcion from "../components/adopta/requisitos";
 import PerrosDisponibles from "../components/adopta/perrosDisponibles";
 import CambioSection from "../components/adopta/CambioSection";
-import { motion } from "framer-motion";
 
 import heroAdopta from "../assets/img/perro5.png"; // TODO: reemplazar img
 

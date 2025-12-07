55c7c4985a2ec15aaa2ed4709fb32a4929cb6026
cam-vasquez <00065221@uca.edu.sv>
2025-12-06 19:42:45 -0600

    :fire: Refactors hero sections and updates images

--- echaunapata/src/pages/apadrina.tsx
+++ echaunapata/src/pages/apadrina.tsx
@@ -1,29 +1,21 @@
 import Navbar from "../components/commons/navbar";
 import Footer from "../components/commons/footer";
-import Hero from "../components/commons/hero";
 import InformacionDonacion from "../components/apadrina/informacionDonacion";
 import MetodosDonacion from "../components/apadrina/metodosDonacion";
-import { motion } from "framer-motion";
+import MiniHero from "../components/commons/miniHero";
+import heroApradrina from "../assets/img/perro1.png";
 
 export default function Apadrina() {
   return (
     <>
       <Navbar/>
-      <Hero>
-        <motion.div
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.6 }}
-          className="max-w-2xl mx-auto flex flex-col items-center justify-center gap-4 text-center"
-        >
-          <h1 className="text-5xl md:text-6xl lg:text-7xl font-bold text-white mb-6 leading-tight">
-            Doná y compartí esperanza
-          </h1>
-          <p className="text-lg md:text-xl text-white/90 mb-8 leading-relaxed">
-            ayudános a cuidar de los peluditos
-          </p>
-        </motion.div>
-      </Hero>
+          <MiniHero
+              media={heroApradrina}
+              title="  Denunciá y protegé vidas"
+              subtitle="Reportá casos de riesgo y ayudanos a proteger a los peluditos."
+              align="center"
+            >
+            </MiniHero>
       <InformacionDonacion />
       <MetodosDonacion />
       <Footer />
--- echaunapata/src/pages/hubNetwork.tsx
+++ echaunapata/src/pages/hubNetwork.tsx
@@ -1,10 +1,19 @@
 import Navbar from "../components/commons/navbar";
 import Footer from "../components/commons/footer";
+import MiniHero from "../components/commons/miniHero";
+import heroHubNetwork from "../assets/img/perro1.png";
 
 export default function HubNetwork() {
   return (
     <>
       <Navbar />
+        <MiniHero
+              media={heroHubNetwork}
+              title="Madre de refugio"
+              subtitle="Red de apoyo para peluditos en situación de calle"
+              align="center"
+            >
+            </MiniHero>
       {/* Add your HubNetwork page content here */}
       <Footer />
     </>
